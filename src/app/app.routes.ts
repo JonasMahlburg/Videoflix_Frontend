a0c125a8bf5511abc9a3e20ee79b20e66f4814e1
JonasMahlburg <darrylmanga@yahoo.com>
2025-08-18 16:22:45 +0200

    feat: added LogIn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { Imprint } from './imprint/imprint';
+import { LogInPage } from './log-in-page/log-in-page';
 
 export const routes: Routes = [
     {path: '', component: LandingPage},
     {path: 'Impressum', component: Imprint},
+    {path: 'login', component: LogInPage}
 ];
---
+++ src/app/log-in-page/log-in-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-page',
+  imports: [],
+  templateUrl: './log-in-page.html',
+  styleUrl: './log-in-page.scss'
+})
+export class LogInPage {
+  showPw(){
+        alert:'Hallo Welt'
+      }
+}
