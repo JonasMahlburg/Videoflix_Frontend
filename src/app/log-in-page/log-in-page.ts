b7f080d8ee8f8af01dd36a7ca1902456d739b167
JonasMahlburg <darrylmanga@yahoo.com>
2025-08-19 10:18:42 +0200

    update Login

--- src/app/log-in-page/log-in-page.ts
+++ src/app/log-in-page/log-in-page.ts
@@ -6,8 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './log-in-page.html',
   styleUrl: './log-in-page.scss'
 })
+
 export class LogInPage {
-  showPw(){
-        alert:'Hallo Welt'
-      }
+  showPw() {
+    let pwInput = document.getElementById("PwInput") as HTMLInputElement;
+    let pwIcon = document.getElementById("pwIcon") as HTMLImageElement;
+
+    if (pwInput.type === "password") {
+      pwInput.type = "text";
+      pwIcon.src = "icons/visibility_off.svg";
+    } else {
+      pwInput.type = "password";
+      pwIcon.src = "icons/visibility.svg";
+    }
+  }
 }
